3b1f380d8606c2a63bdfea16e862231fb72454f8
michael jones <michael.jowen.jones@gmail.com>
2021-07-29 21:48:18 +0100

    good

--- challenges/0-document.js
+++ challenges/0-document.js
@@ -1,7 +1,16 @@
 // TODO (Ex.0): document and write tests for this function
-
+/**
+ * Returns the negative version of the number
+ * 
+ * @param {number} n - the number to negate
+ * 
+ * @returns {number} the negative version of the number 'n'
+ */
 function asNegative(n) {
-  // ...
+  if (n > 0){
+    return -n
+  }
+  return n
 }
 
 
